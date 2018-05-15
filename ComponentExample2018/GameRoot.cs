7a4d2bacbc0e80dc9efc3f44dee7071caf6a14da
s00172994 <david.brennan@mail.itsligo.ie>
2018-05-15 19:34:26 +0100

    GameRoot class formatting.

--- ComponentExample2018/GameRoot.cs
+++ ComponentExample2018/GameRoot.cs
@@ -24,7 +24,6 @@ namespace ComponentExample2018
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
             new InputEngine(this);
             new Camera(this, Vector2.Zero, GraphicsDevice.Viewport.Bounds.Size.ToVector2());
             IsMouseVisible = true;
@@ -36,23 +35,24 @@ namespace ComponentExample2018
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
             Services.AddService(spriteBatch);
+
             // This will load a set of Enemy Images into the static dictionary 
             GameUtilities.sGameTextures = Loader.ContentLoad<Texture2D>(Content, "Images");
+
             // To add additional images from a different location to the same Dictionary
             foreach (var tx in Loader.ContentLoad<Texture2D>(Content, "Images\\Textures"))
             {
                 GameUtilities.sGameTextures.Add(tx.Key, tx.Value);
             }
+
             Enemy e1 = new Enemy(this, "Enemy1", new Vector2(100, 100));
 
-            // load the audio in the Audio Manager
+            // Load the audio in the Audio Manager
             AudioManager.SoundEffects = Loader.ContentLoad<SoundEffect>(Content, "Audio");
-            // TODO: use this.Content to load your game content here
         }
     
         protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
         }
 
         protected override void Update(GameTime gameTime)
@@ -60,7 +60,7 @@ namespace ComponentExample2018
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+
 
             base.Update(gameTime);
         }
@@ -68,10 +68,12 @@ namespace ComponentExample2018
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+
             spriteBatch.Begin();
+
             spriteBatch.Draw(GameUtilities.sGameTextures["islandicon_2"], GraphicsDevice.Viewport.Bounds, Color.White);
+
             spriteBatch.End();
-            // TODO: Add your drawing code here
 
             base.Draw(gameTime);
         }
